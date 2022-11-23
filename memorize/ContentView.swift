9a39dc1738f9fad4d9e8b9b0ada9e5398c768708
hmarques <henrique.marques2@fatec.sp.gov.br>
2022-11-23 00:24:22 -0300

    second way to write code in swift

--- memorize/ContentView.swift
+++ memorize/ContentView.swift
@@ -12,19 +12,31 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        HStack(spacing: 0, content: {
-            ForEach(0..<4, content: {
-                index in
-                ZStack(content: {
-                    RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
-                    RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
-                    Text("👻")
-                })
-            })
-        })
+        HStack {
+            ForEach(0..<4) { index in
+                CardView(isFaceUp: true)
+            }
+        }
         .padding()
         .foregroundColor(Color.orange)
         .font(Font.largeTitle)
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool
+    
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+                Text("👻")
+            } else{
+                RoundedRectangle(cornerRadius: 10.0).fill()
+            }
+            
+        }
     }
 }
 
