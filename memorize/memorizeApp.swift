4fc65f87cbdc663b34c85054b1b445a7570c94e6
hmarques <henrique.marques2@fatec.sp.gov.br>
2022-11-22 22:44:06 -0300

    Initial Commit

---
+++ memorize/memorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  memorizeApp.swift
+//  memorize
+//
+//  Created by Henrique Marques on 22/11/22.
+//
+
+import SwiftUI
+
+@main
+struct memorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
